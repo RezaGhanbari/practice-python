73c8c66deed3f7aab7c7c6c65ee2c248e3563042
Reza <rezapython@gmail.com>
2016-11-03 22:30:49 +0330

    Refactoring file names.

---
+++ Scrapping/Introduction/link_crawler_1.py
@@ -0,0 +1,15 @@
+import sys
+from link_crawler import *
+import urllib2
+import urlparse
+
+
+def link_crawler_two(seed_url, link_regx):
+    """"
+    Crawl from the given seed URL following links matched by link_regx
+    """
+    crawl_queue = [seed_url]
+    while crawl_queue:
+        url = crawl_queue.pop()
+        html = download(url)
+        for link in get_links
