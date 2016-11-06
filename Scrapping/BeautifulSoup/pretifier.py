f1411a1f4b9ce164cc17668b9f58f25dfe5ce79e
Reza <rezapython@gmail.com>
2016-11-06 12:29:23 +0330

    Sample source pretiffier.

---
+++ Scrapping/BeautifulSoup/pretifier.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+r = requests.post(url=raw_input('Please insert a valid url:\n'))
+s = BeautifulSoup(r.text, 'lxml')
+# broken_html = raw_input('paste the
+# parse the html
+fixed = s.prettify()
+s = fixed.encode('ascii','ignore')
+f1 = open('./testfile.txt', 'w+')
+f1.write(s)
--- webcrawler/scrapper.py
+++ webcrawler/scrapper.py
@@ -6,3 +6,8 @@ headers = {}
 html = download(url, headers=headers, proxy=None, num_retries=5)
 print re.findall('<td class="w2p_fw">(.*?)</td>', html)
 # print re.findall('<td class="w2p_fw">(.*?)</td>', html)[1]
+
+# more robust item
+# re.findall('<tr id="places_area__row"><td class="w2p_fl">
+# <label for="places_area" id="places_area__label">
+# Area: </label></td><td class="w2p_fw">(.*?)</td>', html)
