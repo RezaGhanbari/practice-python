148a53e267b04b0827e80c2be35571f2a8d4ae01
Reza <rezapython@gmail.com>
2016-11-06 12:01:14 +0330

    added some basic scrapper

---
+++ webcrawler/scrapper.py
@@ -0,0 +1,7 @@
+from crawler import *
+import re
+
+url = 'http://example.webscraping.com/view/UnitedKingdom-239'
+headers = {}
+html = download(url, headers=headers, proxy=None, num_retries=5)
+print re.findall('<td class="w2p_fw">(.*?)</td>', html)
