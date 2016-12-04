129ef2adc4f63d18fc6eaaa416982c4593d54104
Reza Ghanbari <rezapython@gmail.com>
2016-12-04 13:01:44 +0330

    Add files via upload

--- tram.py
+++ tram.py
@@ -1,22 +1,5 @@
-n=input()
-a=[]
-for i in range(n):
-  a.append(raw_input().split(' '))
-x,y=[],[]
-
-for i in a:
-  x.append(int(i[0]))
-  y.append(int(i[1]))
-
-s=[]
-
-for i in range(len(y)):
-  if i==(len(y)-1):
-    break;
-  s.append(y[i]+y[i+1])
-t=[]
-x.remove(x[0])
-for j in range(len(s)):
-  t.append(s[j]-x[j])
-print t
-print max(t)
+m=k=0
+for i in'0'*input():
+    k-=eval(raw_input().replace(' ','-'))
+    m=max(m,k)
+print(m)
