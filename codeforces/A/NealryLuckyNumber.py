b4c27ab23f9aa190c26855b6dbfb8455228b6cb7
Reza Ghanbari <rezapython@gmail.com>
2017-01-13 23:20:07 +0330

    amend

--- codeforces/A/NealryLuckyNumber.py
+++ codeforces/A/NealryLuckyNumber.py
@@ -1,16 +1,4 @@
 from __future__ import print_function
 
-
-def lucky(n):
-    return True if any(set("47") >= set(str(i)) and n % i == 0 for i in range(1, n + 1))else False
-
-
-def get_number(n):
-    c = []
-    for i in n:
-        if eval(i)==7 or eval(i)==4:
-            c.append(1)
-    return sum(c)
-
-n = raw_input()
-print("YES" if lucky(get_number(n)) else "NO")
+s = raw_input()
+print(['YES', 'NO'][s.count('4') + s.count('7') not in [4, 7]])
